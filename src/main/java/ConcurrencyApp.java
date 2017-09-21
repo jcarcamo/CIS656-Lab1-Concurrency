179abb4dae4935b5578f66abbc25f9f903373238
Juan Gonzalo Carcamo <jcarcamo@gmail.com>
2017-09-21 15:08:34 -0400

    trying to make it less cluttered

--- src/main/java/ConcurrencyApp.java
+++ src/main/java/ConcurrencyApp.java
@@ -6,12 +6,76 @@ import model.ConcurrencyB;
 import utils.Counter;
 
 public class ConcurrencyApp{
+	enum TYPE{
+		A,
+		B;
+		
+		private ConcurrencyA concurrencyA;
+		private ConcurrencyB concurrencyB;
+		
+		public void setCounter (Counter counter){
+			this.concurrencyA = new ConcurrencyA(counter);
+			this.concurrencyB = new ConcurrencyB(counter);
+		}
+		
+		public ConcurrencyA getConcurrencyA(){
+			return concurrencyA;
+		}
+		public ConcurrencyB getConcurrencyB(){
+			return concurrencyB;
+		}
+	}
 	
+	public void runConcurrencyTest (int threadCount, TYPE type, Counter sharedCounter){
+		sharedCounter.setProgramCounter(0);
+		type.setCounter(sharedCounter);
+		List<Thread> threads = new ArrayList<>();
+		for(int i=0; i< threadCount; i++){
+			switch(type){
+			case A:
+				threads.add(new Thread(type.getConcurrencyA()));
+				break;
+			case B:
+				threads.add(new Thread(type.getConcurrencyB()));
+				break;
+			}
+			
+		}
+		long startTime = System.currentTimeMillis();	
+		for(Thread thread:threads){
+			thread.start();
+		}
+
+		try {
+			for(Thread thread:threads){
+				thread.join();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long finalTime = System.currentTimeMillis() - startTime;
+		StringBuilder outputSb = new StringBuilder();
+		switch(type){
+		case A:
+			outputSb.append("Program A:").append('\n');
+			break;
+		case B:
+			outputSb.append("Program B:").append('\n');
+			break;
+		}
+		
+		outputSb.append("Number of threads: ").append(threadCount).append('\n');
+		outputSb.append("Execution Time: ").append(finalTime).append(" milliseconds").append('\n');
+		outputSb.append("Counter Value: ").append(sharedCounter.getProgramCounter()).append('\n');
+		outputSb.append("********************************").append("\n\n\n");
+		System.out.println(outputSb.toString());
+
+		
+		
+	}
 	public static void main(String[] args) {
 		Counter sharedCounter = new Counter();
-		
 		int threadCount = 0;
-		List<Thread> threads = new ArrayList<>();
 		
 		try{
 			threadCount = Integer.parseInt(args[0]);
@@ -27,54 +91,19 @@ public class ConcurrencyApp{
 			System.exit(1);
 		}
 		
-		for(int i=0; i< threadCount; i++){
-			threads.add(new Thread(new ConcurrencyA(sharedCounter)));
-		}
-		
-		long startTime = System.currentTimeMillis();	
-		for(Thread thread:threads){
-			thread.start();
-		}
-
-		try {
-			for(Thread thread:threads){
-				thread.join();
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		long finalTime = System.currentTimeMillis() - startTime;
-		StringBuilder outputSb = new StringBuilder();
-		outputSb.append("Program A:").append('\n');
-		outputSb.append("Number of threads: ").append(threadCount).append('\n');
-		outputSb.append("Execution Time: ").append(finalTime).append(" milliseconds").append('\n');
-		outputSb.append("Counter Value: ").append(sharedCounter.getProgramCounter()).append('\n');
-		outputSb.append("********************************").append("\n\n\n");
-		
-		threads.clear();
-		sharedCounter.setProgramCounter(0);
-		for(int i=0; i< threadCount; i++){
-			threads.add(new Thread(new ConcurrencyB(sharedCounter)));
-		}
-		
-		startTime = System.currentTimeMillis();	
-		for(Thread thread:threads){
-			thread.start();
-		}
-
-		try {
-			for(Thread thread:threads){
-				thread.join();
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		finalTime = System.currentTimeMillis() - startTime;
-		outputSb.append("Program B:").append('\n');
-		outputSb.append("Number of threads: ").append(threadCount).append('\n');
-		outputSb.append("Execution Time: ").append(finalTime).append(" milliseconds").append('\n');
-		outputSb.append("Counter Value: ").append(sharedCounter.getProgramCounter());
-		System.out.println(outputSb.toString());
+		ConcurrencyApp app = new ConcurrencyApp();
+		TYPE type = TYPE.A;
+		app.runConcurrencyTest(threadCount, type,sharedCounter);
+		type = TYPE.B;
+		app.runConcurrencyTest(threadCount, type,sharedCounter);
+		type = TYPE.A;
+		app.runConcurrencyTest(threadCount, type,sharedCounter);
+		type = TYPE.B;
+		app.runConcurrencyTest(threadCount, type,sharedCounter);
+		type = TYPE.A;
+		app.runConcurrencyTest(threadCount, type,sharedCounter);
+		type = TYPE.B;
+		app.runConcurrencyTest(threadCount, type,sharedCounter);
 
     }
 }
